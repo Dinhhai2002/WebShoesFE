639ad2f27b385e3932cda1e61ff3af90bd0dcb28
Dinhhai2002 <88237144+Dinhhai2002@users.noreply.github.com>
2025-03-08 15:54:53 +0700

    feature : add page login

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,31 @@ import Product from "./pages/ProductDetail";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import ProductList from "./pages/ProductList";
-
+import { CartProvider } from "./context/CartContext";
+// import { customTheme } from "./style/themeCustom";
+import { ToastContainer } from "../node_modules/react-toastify/dist/index";
+import Login from "./pages/Login/Login";
+import { AuthProvider } from "./context/AuthContext";
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline /> {/* Reset CSS để đồng bộ */}
+      <ToastContainer />
       <Router>
+      <AuthProvider>
+      <CartProvider>
         <Header />
         <Routes>
-          {/* <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/authentication/login" element={<Login />} />
           <Route path="/product/:id" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
-          <Route path="/list-product" element={<ProductList />} /> */}
+          <Route path="/list-product" element={<ProductList />} />
         </Routes>
         <Footer />
+        </CartProvider>
+        </AuthProvider>
       </Router>
     </ThemeProvider>
   );
